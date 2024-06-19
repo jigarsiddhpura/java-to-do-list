2c73ac5441ce252bf8b6ca7eadced5bf2adbee69
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-06-19 20:06:18 +0530

    feat : display all tasks

--- src/main/java/dev/jigar/Features/DisplayTasks.java
+++ src/main/java/dev/jigar/Features/DisplayTasks.java
@@ -1,5 +1,8 @@
 package dev.jigar.Features;
 
+import java.util.Map;
+
+import dev.jigar.App.Task;
 import dev.jigar.App.ToDoList;
 
 public class DisplayTasks extends Actions{
@@ -16,8 +19,13 @@ public class DisplayTasks extends Actions{
 
     @Override
     public void executeAction(String command){
-        ToDoList.tasks.forEach((key, task) -> {
+        // ToDoList.tasks.forEach((key, task) -> {
+        //     System.out.println("ID: " + key + ", Title: " + task.getTitle() + ", Due date: " + task.getDueDate() + ", Status: " + task.getStatus() + ", Description: " + task.getDescription());
+        // });
+        for(Map.Entry<String,Task> entry : ToDoList.tasks.entrySet()) {
+            String key = entry.getKey();
+            Task task = entry.getValue();
             System.out.println("ID: " + key + ", Title: " + task.getTitle() + ", Due date: " + task.getDueDate() + ", Status: " + task.getStatus() + ", Description: " + task.getDescription());
-        });
+        }
     }
 }
